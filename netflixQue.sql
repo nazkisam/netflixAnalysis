1e4a2edd6185fafdb8052530da85246ea963d64e
Sameem <nsam603@gmail.com>
2025-02-02 21:10:14 +0530

    netflix_table populated

--- netflixQue.sql
+++ netflixQue.sql
@@ -33,3 +33,45 @@ FROM 'D:\PLCPP\SQL\newproject\netflix\netflix_titles.csv'
 DELIMITER ','
 CSV HEADER;
  
+
+
+ SELECT *FROM netflix;
+
+--1. Count the Number of Movies vs TV Shows
+    --1st solution
+SELECT COUNT(type) AS MOVIES, COUNT(type)AS tv_shows
+FROM netflix
+WHERE type = 'Movie' OR type = 'TV Show';
+
+ -- 2nd solution
+SELECT 
+    type,
+    COUNT(*)
+FROM netflix
+GROUP BY 1;
+
+
+
+
+
+
+
+
+
+
+
+
+-- 2. Find the Most Common Rating for Movies and TV Shows
+-- 3. List All Movies Released in a Specific Year (e.g., 2020)
+-- 4. Find the Top 5 Countries with the Most Content on Netflix
+-- 5. Identify the Longest Movie
+-- 6. Find Content Added in the Last 5 Years
+-- 7. Find All Movies/TV Shows by Director 'Rajiv Chilaka'
+-- 8. List All TV Shows with More Than 5 Seasons
+-- 9. Count the Number of Content Items in Each Genre
+-- 10.Find each year and the average numbers of content release in India on netflix.
+-- 11. List All Movies that are Documentaries
+-- 12. Find All Content Without a Director
+-- 13. Find How Many Movies Actor 'Salman Khan' Appeared in the Last 10 Years
+-- 14. Find the Top 10 Actors Who Have Appeared in the Highest Number of Movies Produced in India
+-- 15. Categorize Content Based on the Presence of 'Kill' and 'Violence' Keywords
